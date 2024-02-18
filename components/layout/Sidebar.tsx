54036a895a94550f96790cedffb36e80fe3dbebe
Rituparna Warwatkar <rituparnaw16@gmail.com>
2024-02-18 21:46:26 +0530

    Create sidebar and sidebar items

--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,7 +1,54 @@
 import React from 'react';
+import { BsBellFill, BsHouseFill } from 'react-icons/bs';
+import { FaUser } from 'react-icons/fa';
+import SidebarLogo from './SidebarLogo';
+import SidebarItem from './SidebarItem';
+import { BiLogOut } from 'react-icons/bi';
+import SidebarTweetButton from './SidebarTweetButton';
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const items = [
+    {
+      label: 'Home',
+      href: '/',
+      icon: BsHouseFill,
+    },
+    {
+      label: 'Notifications',
+      href: '/',
+      icon: BsBellFill,
+    },
+    {
+      label: 'Profile',
+      href: '/users/123',
+      icon: FaUser,
+    },
+  ];
+  return (
+    <div className="col-span-1 h-full pr-4 md:pr-6">
+      <div className="flex flex-col items-end">
+        <div className="space-y-2 lg:w-[230px]">
+          <SidebarLogo />
+          {items.map((item) => {
+            return (
+              <SidebarItem
+                key={item.href}
+                href={item.href}
+                label={item.label}
+                icon={item.icon}
+              />
+            );
+          })}
+          <SidebarItem
+            label="Logout"
+            icon={BiLogOut}
+            onClick={() => {}}
+          />
+          <SidebarTweetButton />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Sidebar;
