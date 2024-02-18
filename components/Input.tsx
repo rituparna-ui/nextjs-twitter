9235283812c08043cbaf7359e71cbe0d92c8b527
Rituparna Warwatkar <rituparnaw16@gmail.com>
2024-02-19 00:12:21 +0530

    Create a custom input component and login modal store

---
+++ components/Input.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+
+interface IInputProps {
+  placeholder?: string;
+  value?: string;
+  type?: string;
+  disabled?: boolean;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+const Input: React.FC<IInputProps> = ({
+  disabled,
+  onChange,
+  placeholder,
+  type,
+  value,
+}) => {
+  return (
+    <input
+      disabled={disabled}
+      onChange={onChange}
+      placeholder={placeholder}
+      type={type}
+      value={value}
+      className="
+        w-full
+        p-4
+        text-lg
+        bg-black
+        border-2
+        border-neutral-800
+        rounded-md
+        outline-none
+        text-white
+        focus:border-sky-500
+        focus:border-2
+        transition
+        disabled:bg-neutral-900
+        disabled:opacity-70
+        disabled:cursor-not-allowed
+      "
+    />
+  );
+};
+
+export default Input;
---
+++ hooks/useLoginModal.ts
@@ -0,0 +1,17 @@
+import { create } from 'zustand';
+
+interface ILoginModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useLoginModal = create<ILoginModalStore>((set) => {
+  return {
+    isOpen: false,
+    onOpen: () => set({ isOpen: true }),
+    onClose: () => set({ isOpen: false }),
+  };
+});
+
+export default useLoginModal;
